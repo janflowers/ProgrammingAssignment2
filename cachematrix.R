59871ac0d866aeadf52762babb554fd0b9fcea23
janflowers <j9.flowers@gmail.com>
2015-06-21 13:06:28 -0700

    Updating file for programming assignment cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions that take a matrix object and cache the inverse of that matrix,
+## in order to avoid having to perform intensive computing of that inverse on-demand
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix(matrix) creates methods associated with the original matrix
+## to set, get for both the original and the inverse matrix
 
+makeCacheMatrix <- function(m = matrix()) {
+
+  ## initiate inverse of original matrix
+  matrixInverse <- NULL
+  
+  ## set original matrix and clears out any cache of the inverse of the original matrix
+  set <- function(y){
+      m <<- y
+      matrixInverse <<- NULL
+  }
+  
+  ## get (return) the original matrix
+  get <- function() m
+  
+  ## sets but doesn't solve for the inverse
+  setInverse <- function(mInverse) matrixInverse <<- mInverse
+  
+  ## return the cached inverse, return null if not cached yet 
+  getInverse <- function() matrixInverse
+  
+  ## return a list of methods
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(matrix) takes matrix, checks for previous cache, 
+## computes inverse, and returns an inverse of the matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+  
+  ## Return a matrix that is the inverse of 'x'
+  ## Look to see if the inverse matrix is already cached
+  mInverse <- m$getInverse()
+  if(!is.null(mInverse)){
+      message("getting cached matrix inverse")
+      return(mInverse)
+  }
+  
+  ## If not cached, then create inverse, cache, and return
+  data <- m$get()
+  mInverse <- solve(data, ...)
+  m$setInverse(mInverse)
+  mInverse
+  
 }
+
+
